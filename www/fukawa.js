40312131b3de6fbcd3515f6f872181e6bbe22589
kabegin <kabegin@gmail.com>
2017-01-29 10:59:24 +0900

    ook

--- www/fukawa.js
+++ www/fukawa.js
@@ -31,8 +31,8 @@ function fukawa(){
     add.setHours(0, 0, 0, 0);
     var nday=new Date(ad.getTime()+28*24*60*60*1000);
    //alert(add);
-    var k = '<input type="button" value="前のページへ戻る" onclick="location.reload(true)">個別予定表';
-     k += '<ons-scroller><ons-list><ons-listheader>年月日曜予定</ons-list-header>'; 
+//    var k = '<input type="button" value="前のページへ戻る" onclick="location.reload(true)">個別予定表';
+    var k = '<ons-scroller><ons-list><ons-listheader>年月日曜予定</ons-list-header>'; 
      do{   
       day = new Date(hi(add));//iには'2007/5/5':'PHP攻略完了'の形式の前の方の文字列が入っている。
       w = week[day.getDay()];//曜日を取得
@@ -68,11 +68,12 @@ function fukawa(){
     if(result[1]){
     var r1=("0"+result[1].replace(":","")).slice(-4);
     var r2=("0"+result[2].replace(":","")).slice(-4);
-    var tako=hii(add)+r1+"00"+hii(add)+r2+"00";
+    var tako=hii(add)+"T"+r1+"00"+"/"+hii(add)+"T"+r2+"00";
    }else{
-   var tako=hii(add);
+       var pqr=new Date(add.getTime()+24*60*60*1000);
+       var tako=hii(add)+"/"+hii(pqr);
    }
-       k += c+"tit="+tako+" onclick='al(this);'  tappable>"+hi(add).slice(5) + w +deban+maime+" "+job +"</ons-list-item>";
+       k += c+"tit="+tako+" tic="+deban+maime+" onclick='al(this);'  tappable>"+hi(add).slice(5) + w +deban+maime+" "+job +"</ons-list-item>";
       
       if (((add.getTime()-ad.getTime())/(24*60*60*1000))%28!=0){deban+=1;}else{deban=deban;}
       aday(add);
@@ -84,5 +85,12 @@ function fukawa(){
 }
 function al(obj){
     var title = obj.getAttribute('tit');
-alert ( title );
+    var tic=obj.getAttribute('tic');
+/*alert ( title );
+     var modal = document.getElementById('modal');
+     var te = document.getElementById('te');  
+   te.innerHTML=title;
+  modal.toggle();
+  */
+  location.href ="http://www.google.com/calendar/event?action=TEMPLATE&text="+tic+"&dates="+title;
 }
